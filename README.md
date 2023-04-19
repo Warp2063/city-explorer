6120d09dd3b641dc0faece7235ab861932319313
warp2063 <warp2063@gmail.com>
2023-04-19 15:23:57 -0400

    basic page working

--- README.md
+++ README.md
@@ -11,6 +11,8 @@
 
 <!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
 
+`npm install`
+
 ## Architecture
 
 <!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [resultLocation, setResultLocation] = useState({});
+
+  const APIEndpt = "https://us1.locationiq.com";
+  
+
+  function handleQueryChange(event) {
+    setSearchQuery(event.target.value);
+  }
+  
+  async function requestLocation() {
+    const queryStr = APIEndpt + "/v1/search.php?key=" + process.env.REACT_APP_API_KEY + 
+    "&q=" + searchQuery + "&format=json";
+    const response = await axios.get(queryStr);
+    setResultLocation(response.data[0]);
+  }
+
   return (
     <div className="App">
-      Stuffs in App.js
+      <input onChange={handleQueryChange} />
+      <button onClick={requestLocation}>Get Location!</button>
+      <h1>The requested location is: {resultLocation.display_name}</h1>
     </div>
   );
 }
